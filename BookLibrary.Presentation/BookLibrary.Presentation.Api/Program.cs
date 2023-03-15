0cfeff677492cae4d706f4aa5dd37776b7aaa00f
ilayda <ilaydaozken@gmail.com>
2023-03-15 19:05:33 +0300

    ,

--- BookLibrary.Presentation/BookLibrary.Presentation.Api/Program.cs
+++ BookLibrary.Presentation/BookLibrary.Presentation.Api/Program.cs
@@ -1,10 +1,8 @@
-using AutoMapper;
 using BookLibrary.Core.Application;
-using BookLibrary.Core.Application.Services.Commands.Book.Insert;
-using BookLibrary.Core.Application.Services.Queries.Book.GetById;
 using BookLibrary.Infrastructure.Persistence;
 using MediatR;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,16 +11,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins("https://localhost:7298").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddApplicationServices();
 builder.Services.AddMediatR(typeof(Program) );
 
 var app = builder.Build();
-
-
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,6 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
